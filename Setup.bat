9690f3554866a95167b15bd3c6a72e504ffc31b4
Henry-NetAss <142622280+Henry-NetAss@users.noreply.github.com>
2024-01-02 11:20:11 +0200

    Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -1,11 +1,34 @@
-@echo off
-cd c:\NA\Installation
-powershell.exe c:\na\installation\Region_Settings.ps1
+powershell -command "& {Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force}"
+powershell.exe C:\NA\Installation\UAC_Disable.ps1
+powershell.exe C:\NA\Installation\DarkMode.ps1
+powershell.exe C:\NA\Installation\Region_Settings.ps1
+powershell.exe C:\NA\Installation\Disable_Fast_Boot.ps1
 tzutil /s "South Africa Standard Time"
+Powershell.exe C:\na\Installation\RequestPCName.ps1
+cls
+Powershell.exe C:\NA\Installation\PCName.ps1
+powershell.exe C:\NA\Installation\software.ps1
+echo Installing Stuff
+powershell.exe C:\NA\Installation\O365BR.ps1
 timeout 5
-chrome.exe /silent /install
-tsprint.exe /silent
-zoom.exe /silent
-timeout 15
+cls
+powershell -command "& {Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force}"
+powershell.exe c:\na\Installation\Desktop_Shortcuts.ps1
+timeout 5
+cls
+echo Installing Stuff
+powershell -command "& {Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force}"
+powershell.exe C:\NA\Installation\Anydesksilent.ps1
+timeout 5
+cls
+echo Installing Stuff
 nettime.exe /silent
+timeout 20
+cls
+tsprint.exe /silent
+timeout 5
+cls
+powershell.exe C:\NA\Installation\remove_printers.ps1
+cls
+powershell.exe C:\NA\Installation\updates.ps1
 exit
