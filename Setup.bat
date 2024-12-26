51debee6d2cdf4671a1136094c050048efbc0340
Henry-NetAss <142622280+Henry-NetAss@users.noreply.github.com>
2024-12-26 21:17:37 +0200

    Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -1,6 +1,5 @@
 cd c:\NA\Installation
 powershell -command "& {Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force}"
-powershell.exe(New-Object -ComObject Shell.Application).MinimizeAll()
 powershell.exe c:\na\installation\website.ps1
 powershell.exe C:\NA\Installation\UAC_Disable.ps1
 powershell.exe C:\NA\Installation\DarkMode.ps1
