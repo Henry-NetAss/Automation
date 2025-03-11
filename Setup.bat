552f0d2a5e55e97c1f7d131d2a1a4ee58b161b47
Henry-NetAss <142622280+Henry-NetAss@users.noreply.github.com>
2025-03-11 20:03:45 +0200

    Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -10,23 +10,17 @@ powershell.exe C:\NA\Installation\Region_Settings.ps1
 powershell.exe C:\NA\Installation\Disable_Fast_Boot.ps1
 tzutil /s "South Africa Standard Time"
 powershell.exe C:\NA\Installation\software.ps1
-
 timeout 5
 powershell.exe C:\NA\Installation\Anydesksilent.ps1
 cls
 powershell -command "& {Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force}"
 powershell.exe c:\na\Installation\Desktop_Shortcuts.ps1
 timeout 5
-cls
-echo Installing Stuff
 powershell -command "& {Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force}"
-
-timeout 5
-cls
 powershell.exe C:\NA\Installation\remove_printers.ps1
 cls
 powershell.exe C:\NA\Installation\NIC_Power_Management.ps1
-timeout 10
+timeout 20
 cls
 powershell.exe C:\NA\Installation\updates.ps1
 exit
