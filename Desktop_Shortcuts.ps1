ad8e064d919977466837e3aaecefb3511b387de9
Henry-NetAss <142622280+Henry-NetAss@users.noreply.github.com>
2025-03-12 12:32:45 +0200

    Desktop_Shortcuts.ps1

--- Desktop_Shortcuts.ps1
+++ Desktop_Shortcuts.ps1
@@ -1,4 +1,4 @@
 ﻿copy-item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Excel.lnk" -Destination "$env:USERPROFILE\Desktop\Excel.lnk" -Recurse -Force
 copy-item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Word.lnk" -Destination "$env:USERPROFILE\Desktop\Word.lnk" -Recurse -Force
-copy-item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Outlook (Classic).lnk" -Destination "$env:USERPROFILE\Desktop\Outlook (Classic).lnk" -Recurse -Force
+copy-item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Outlook (Classic).lnk" -Destination "$env:USERPROFILE\Desktop\Outlook.lnk" -Recurse -Force
 
