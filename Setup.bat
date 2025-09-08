64cd5ea5894a68cff4537af9c1fe531802c847d7
Henry-NetAss <142622280+Henry-NetAss@users.noreply.github.com>
2025-09-08 15:54:46 +0200

    Update Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -34,11 +34,12 @@ powershell -command "& {Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
 powershell.exe c:\na\Installation\Desktop_Shortcuts.ps1
 powershell.exe c:\na\installation\taskbaricons.ps1
 timeout 5
-Powershell.exe c:\na\installation\PCName.ps1
+# Powershell.exe c:\na\installation\PCName.ps1
 powershell.exe c:\na\Installation\DisableSearchBoxSuggestion.ps1
 powershell.exe C:\NA\Installation\Region_Settings.ps1
 powershell.exe C:\NA\Installation\updates.ps1
 
 exit
+
 
 
