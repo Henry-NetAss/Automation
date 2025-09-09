b0eca02a7f21beff229ccfc47e11b0b1eb9cc962
Henry-NetAss <142622280+Henry-NetAss@users.noreply.github.com>
2025-09-09 14:15:34 +0200

    Update Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -1,6 +1,5 @@
 cd c:\NA\Installation
 powershell -command "& {Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force}"
-timeout 50
 # powershell.exe C:\na\Installation\RequestPCName.ps1
 powercfg -setdcvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 2
 powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 2
@@ -15,7 +14,7 @@ powercfg -change -monitor-timeout-dc 10
 powercfg -change -standby-timeout-dc 30
 powercfg -change -hibernate-timeout-dc 30
 powershell -command "& {Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force}"
-powershell.exe c:\na\installation\winget_enable.ps1
+# powershell.exe c:\na\installation\winget_enable.ps1
 powershell.exe c:\na\installation\VisualFX.ps1
 powershell.exe C:\NA\Installation\UAC_Disable.ps1
 powershell.exe C:\NA\Installation\DarkMode.ps1
@@ -37,9 +36,10 @@ timeout 5
 # Powershell.exe c:\na\installation\PCName.ps1
 powershell.exe c:\na\Installation\DisableSearchBoxSuggestion.ps1
 powershell.exe C:\NA\Installation\Region_Settings.ps1
-powershell.exe C:\NA\Installation\updates.ps1
+# powershell.exe C:\NA\Installation\updates.ps1
 
 exit
+
 
 
 
